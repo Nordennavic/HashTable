704e12d03638980f2a8d07e079a50abb766896d9
Денис Ткачук <liptonb@ya.ru>
2019-03-28 22:54:31 +0500

    Добавьте файлы проекта.

---
+++ HashTable/HashTable.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    public class HashTable
+    {
+        private List<HashPair>[] table;
+        class HashPair
+        {
+            public int Key;
+            public object Value;
+            public HashPair(int key, object value)
+            {
+                Key = key;
+                Value = value;
+            }
+        }
+
+        /// <summary>
+        /// Конструктор контейнера
+        /// <summary>
+        /// size">Размер хэ-таблицы
+        public HashTable(int size)
+        {
+            table = new List<HashPair>[size];
+        }
+
+
+
+        ///
+        /// Метод складывающий пару ключ-значение в таблицу
+        ///
+        /// key">
+        /// value">
+        public void PutPair(object key, object value)
+        {
+            var hashCode = key.GetHashCode();
+            var ind = Math.Abs(hashCode) % table.Length;
+            if (table[ind] == null)
+            {
+                table[ind] = new List<HashPair> { new HashPair(hashCode, value) };
+            }
+            else
+            {
+                var trigger = true;
+                foreach (var e in table[ind])
+                {
+                    if (e.Key == hashCode)
+                    {
+                        e.Value = value;
+                        trigger = false;
+                        break;
+                    }
+                }
+                if (trigger) table[ind].Add(new HashPair(hashCode, value));
+            }
+        }
+
+
+
+        /// <summary>
+        /// Поиск значения по ключу
+        /// summary>
+        /// key">Ключь
+        /// <returns>Значение, null если ключ отсутствует returns>
+        public object GetValueByKey(object key)
+        {
+            try
+            {
+                var seekingHashCode = key.GetHashCode();
+                var seekingValue = table[Math.Abs(seekingHashCode) % table.Length];
+                return seekingValue.Find(x => x.Key == seekingHashCode).Value;
+            }
+            catch
+            {
+                return null;
+            }
+
+
+        }
+
+    }
+}
---
+++ HashTable/HashTableTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.ComponentModel;
+using NUnit.Framework;
+
+//using HashTable = System.Collections.Generic.Dictionary<object, object>;
+
+namespace HashTable
+{
+    [TestFixture]
+    public class HashTableTests
+    {
+        /// <summary>
+        /// Добавление трёх элементов, поиск трёх элементов
+        /// </summary>
+        [Test]
+        public void AddingThreeElementsTest()
+        {
+            var element1 = 123;
+            var element2 = "element2";
+            var element3 = true;
+            var key1 = "element";
+            var key2 = false;
+            var key3 = 123;
+            var ht = new HashTable(3);
+            ht.PutPair(key1, element1);
+            ht.PutPair(key2, element2);
+            ht.PutPair(key3, element3);
+            Assert.AreEqual(element1, ht.GetValueByKey(key1));
+            Assert.AreEqual(element2, ht.GetValueByKey(key2));
+            Assert.AreEqual(element3, ht.GetValueByKey(key3));
+        }
+
+        /// <summary>
+        /// Добавление одного и того же ключа дважды с разными значениями сохраняет последнее добавленное значение
+        /// </summary>
+        [Test]
+        public void AddingSameElementTwiceTest()
+        {
+            var element2 = "previous";
+            var newValue = "next";
+            var key2 = 0;
+            var ht = new HashTable(3);
+            ht.PutPair(key2, element2);
+            ht.PutPair(key2, newValue);
+            Assert.AreEqual(newValue, ht.GetValueByKey(key2));
+        }
+
+        /// <summary>
+        ///  Добавление 10000 элементов в структуру и поиск одного из них
+        /// </summary>
+        [Test]
+        public void ThousandsElementsTest()
+        {
+            int findingKey = 0;
+            int findingElement = 0;
+            int flag;
+            var size = 10000;
+            var rnd = new Random(10);
+            flag = rnd.Next(size);
+            var ht = new HashTable(size);
+            for (var i = 0; i < size; i++)
+            {
+                var element = rnd.Next();
+                var key = rnd.Next();
+                ht.PutPair(key, element);
+                if (i == flag)
+                {
+                    findingElement = element;
+                    findingKey = key;
+                }
+            }
+            Assert.AreEqual(findingElement, ht.GetValueByKey(findingKey));
+        }
+
+
+        /// <summary>
+        /// Добавление 10000 элементов в структуру и поиск 1000 недобавленных ключей, поиск которых должен вернуть null
+        /// </summary>
+        [Test]
+        public void FindingAlienKeys()
+        {
+            int flag;
+            var size = 10000;
+            var rnd = new Random(270005);
+            flag = rnd.Next(size);
+            var ht = new HashTable(size);
+            for (var i = 0; i < size; i++)
+            {
+                var element = rnd.Next();
+                ht.PutPair(i, element);
+            }
+            for (var i = 0; i < 1000; i++)
+                Assert.AreEqual(null, ht.GetValueByKey(rnd.Next(1000) + 10000));
+        }
+
+    }
+}
